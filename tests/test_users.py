236c15a9c6d35f9b7cf0e154cfccb0de346b11de
Gavrie Philipson <gavriep@il.ibm.com>
2014-06-11 10:32:49 +0300

    updated

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,25 @@
+import pytest
 from modern import users
 
 def test_add_user():
     user = users.create('danny')
     assert user.username == 'danny'
+
+def test_retrieve_user():
+    david = users.create('david')
+    assert users.get('david') == david
+
+def test_retrieve_nonexistent_user():
+    with pytest.raises(LookupError):
+        users.get('david')
+
+def test_user_length():
+    usernames = ['one', 'two', 'three']
+    for index, username in usernames:
+        assert len(users) == index
+        users.create(username)
+
+def test_delete_user():
+    users.create('david')
+    users.delete('david')
+    assert len(users) == 0
