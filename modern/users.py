15c36207348dc4713caf33a5c917ad3e5e42228a
Gavrie Philipson <gavriep@il.ibm.com>
2014-06-11 13:50:12 +0300

    mock

--- modern/users.py
+++ modern/users.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class User(object):
     def __init__(self, username):
         self.username = username
@@ -7,17 +9,24 @@ class User(object):
 
 class Users(object):
     def __init__(self):
-        self.users = set()
+        self.conn = sqlite3.connect("users.db")
+        c = self.conn.cursor()
+        c.execute("create table if not exists users (username text unique)")
 
     def create(self, username):
-        self.users.add(username)
+        c = self.conn.cursor()
+        c.execute("insert into users values (?)", (username,))
         return User(username)
 
     def delete(self, username):
-        self.users.remove(username)
+        c = self.conn.cursor()
+        c.execute("delete from users where username=?", (username,))
 
     def get(self, username):
-        if username in self.users:
-            return User(username)
-        else:
+        c = self.conn.cursor()
+        c.execute("select username from users where username=?", (username,))
+        result = c.fetchone()
+        if not result:
             raise LookupError
+        (username,) = result
+        return User(username)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,13 @@
+import mock
 import pytest
 import modern.users
 
+sqlite3 = mock.MagicMock()
+
+
 @pytest.fixture
 def users():
+    modern.users.sqlite3 = sqlite3
     return modern.users.Users()
 
 def test_add_user(users):
@@ -10,8 +15,14 @@ def test_add_user(users):
     assert user.username == 'danny'
 
 def test_retrieve_user(users):
+    # import pdb; pdb.set_trace()
+    cursor = sqlite3.connect().cursor()
+    assert cursor.execute.call_count == 1
     david = users.create('david')
+    assert cursor.execute.call_count == 2
+    cursor.fetchone.return_value = ('david',)
     assert users.get('david') == david
+    assert cursor.execute.call_count == 3
 
 def test_retrieve_nonexistent_user(users):
     with pytest.raises(LookupError):
